09cdcd3d0fb55fa8c338624ed59763d356f49a9d
Dapow221 <141142350+Dapow221@users.noreply.github.com>
2024-05-27 16:09:36 +0700

    adding card done

--- please/components/Works.vue
+++ please/components/Works.vue
@@ -1,13 +1,129 @@
 <template>
-    <div class="card">
-        <Fieldset legend="Works">
-            <p class="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-        </Fieldset>
-    </div>
+    <Fieldset legend="Works">
+        <div class="media-scroller">
+            <div class="media-element">
+                <Card style="width: 25em; overflow: hidden">
+                    <template #header>
+                        <img alt="user header" src="https://i.scdn.co/image/ab67616d0000b273f0432116d481dd6403f27fe1" />
+                    </template>
+                    <template #title>Moor Thunder</template>
+                    <template #content>
+                        <p class="m-0" style="margin-top: -20px;">
+                            Formed by two man, now stand by four man, Post black metal with some Dreamy, Shoegaze and Post
+                            Rock element based in Malang, ID, Released their First EP “Maestitia” , Second EP “In Solitude”
+                            and then released new project with Oh Noo Records in 2022, Moor still breathing and alive to
+                            bring some fresh from new wave of black metal.
+                        </p>
+                    </template>
+                </Card>
+            </div>
+            <div class="media-element">
+                <Card style="width: 25em; overflow: hidden">
+                    <template #header>
+                        <img alt="user header" src="https://www.tsingapore.com/p/hgkswc.jpg" />
+                    </template>
+                    <template #title>Moor Thunder</template>
+                    <template #content>
+                        <p class="m-0" style="margin-top: -20px;">
+                            Formed by two man, now stand by four man, Post black metal with some Dreamy, Shoegaze and Post
+                            Rock element based in Malang, ID, Released their First EP “Maestitia” , Second EP “In Solitude”
+                            and then released new project with Oh Noo Records in 2022, Moor still breathing and alive to
+                            bring some fresh from new wave of black metal.
+                        </p>
+                    </template>
+                </Card>
+            </div>
+            <div class="media-element">
+                <Card style="width: 25em; overflow: hidden">
+                    <template #header>
+                        <img alt="user header"
+                            src="https://images.gmanews.tv/webpics/2024/01/lauren-tsai_2024_01_19_08_27_22.jpg" />
+                    </template>
+                    <template #title>Moor Thunder</template>
+                    <template #content>
+                        <p class="m-0" style="margin-top: -20px;">
+                            Formed by two man, now stand by four man, Post black metal with some Dreamy, Shoegaze and Post
+                            Rock element based in Malang, ID, Released their First EP “Maestitia” , Second EP “In Solitude”
+                            and then released new project with Oh Noo Records in 2022, Moor still breathing and alive to
+                            bring some fresh from new wave of black metal.
+                        </p>
+                    </template>
+                </Card>
+            </div>
+            <div class="media-element">
+                <Card style="width: 25em; overflow: hidden">
+                    <template #header>
+                        <img alt="user header"
+                            src="https://i.pinimg.com/originals/9b/2d/6e/9b2d6e519d6a60c0c45ca3de95dddb2f.jpg" />
+                    </template>
+                    <template #title>Moor Thunder</template>
+                    <template #content>
+                        <p class="m-0" style="margin-top: -20px;">
+                            Formed by two man, now stand by four man, Post black metal with some Dreamy, Shoegaze and Post
+                            Rock element based in Malang, ID, Released their First EP “Maestitia” , Second EP “In Solitude”
+                            and then released new project with Oh Noo Records in 2022, Moor still breathing and alive to
+                            bring some fresh from new wave of black metal.
+                        </p>
+                    </template>
+                </Card>
+            </div>
+            <div class="media-element">
+                <Card style="width: 25em; overflow: hidden">
+                    <template #header>
+                        <img alt="user header"
+                            src="https://i.pinimg.com/originals/9b/2d/6e/9b2d6e519d6a60c0c45ca3de95dddb2f.jpg" />
+                    </template>
+                    <template #title>Moor Thunder</template>
+                    <template #content>
+                        <p class="m-0" style="margin-top: -20px;">
+                            Formed by two man, now stand by four man, Post black metal with some Dreamy, Shoegaze and Post
+                            Rock element based in Malang, ID, Released their First EP “Maestitia” , Second EP “In Solitude”
+                            and then released new project with Oh Noo Records in 2022, Moor still breathing and alive to
+                            bring some fresh from new wave of black metal.
+                        </p>
+                    </template>
+                </Card>
+            </div>
+            <div class="media-element">
+                <Card style="width: 25em; overflow: hidden">
+                    <template #header>
+                        <img alt="user header"
+                            src="https://i.pinimg.com/originals/9b/2d/6e/9b2d6e519d6a60c0c45ca3de95dddb2f.jpg" />
+                    </template>
+                    <template #title>Moor Thunder</template>
+                    <template #content>
+                        <p class="m-0" style="margin-top: -20px;">
+                            Formed by two man, now stand by four man, Post black metal with some Dreamy, Shoegaze and Post
+                            Rock element based in Malang, ID, Released their First EP “Maestitia” , Second EP “In Solitude”
+                            and then released new project with Oh Noo Records in 2022, Moor still breathing and alive to
+                            bring some fresh from new wave of black metal.
+                        </p>
+                    </template>
+                </Card>
+            </div>
+        </div>
+    </Fieldset>
 </template>
 
 <script>
 </script>
+
+<style>
+.media-scroller {
+    display: grid;
+    grid-auto-flow: column;
+    grid-auto-columns: 18%;
+    overflow-x: auto;
+    scroll-snap-type: mandatory;
+}
+
+.media-element {
+    scroll-snap-align: start;
+}
+
+.media-element img {
+    inline-size: 100%;
+    aspect-ratio: 16 / 9;
+    object-fit: cover;
+}
+</style>
